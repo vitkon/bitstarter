d375aaa8b76168a06b26ebef8face0455c9a5132
Ubuntu <ubuntu@ip-10-145-184-60.ec2.internal>
2013-07-11 17:26:40 +0000

    changes to webjs4

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
+var f = fs.readFile('index.html');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var f = fs.readFile('index.html');
   response.send(f);
 
   //response.send('Hello World 2!');
